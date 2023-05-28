7aad6703803a68fd584e17c5262b0d079469e670
bluejoyq <bluejoy32@gmail.com>
2023-05-29 01:12:01 +0900

    feat: 세마포어를 사용한 강제 부하

--- producer/src/app.service.ts
+++ producer/src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { Semaphore } from 'await-semaphore';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly semaphore = new Semaphore(1);
+  async getHello(): Promise<string> {
+    const release = await this.semaphore.acquire(); // 락 획득
+
+    try {
+      await this.delay(5000); // 5초 대기
+      return 'Sequential response from Nest.js!';
+    } finally {
+      release(); // 락 해제
+    }
+  }
+  private delay(ms: number): Promise<void> {
+    return new Promise((resolve) => setTimeout(resolve, ms));
   }
 }
